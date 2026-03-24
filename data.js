29aadd1799978dd022636888de1533b0d249d0a7
FranAC18 <franklinacero18@hotmail.com>
2026-03-23 22:02:55 -0500

    Correción: Botón de compartir

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const cardData = {
         whatsappMessage: "Hola, me gustaría obtener más información sobre sus servicios.",
         company: "SRWA Cia. Ltda",
         note: "Servicios en operaciones petroleras.",
-        url: "https://srawcialtda.com",
+        url: "https://srawcialtda.vercel.app/",
         address: "Ángel de Ugarte 13-08 y Antonio Cabrera, El Coca, Ecuador", // Texto para SEO
         googleMapsEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d997.4215414496241!2d-76.98256893038555!3d-0.46634069997056093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d7a50dff36abff%3A0x295d6d0a3a9df12e!2sAntonio%20Cabrera%2013-08%2C%20Francisco%20de%20Orellana%20220202!5e0!3m2!1ses!2sec!4v1774043941329!5m2!1ses!2sec"
     },
