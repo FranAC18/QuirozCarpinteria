01962bd50b2737188b76dd393dbfa9f2f9c20ad3
FranAC18 <franklinacero18@hotmail.com>
2026-04-06 03:50:21 -0500

    Update: correccion compartir tarjerta

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const cardData = {
         whatsappMessage: "Hola, me gustaría obtener más información sobre sus servicios.",
         company: "Quiroz Carpintería",
         note: "Servicios en operaciones petroleras.",
-        url: "https://srawcialtda.vercel.app/",
+        url: "https://quiroz-carpinteria.vercel.app",
         address: "Ángel de Ugarte 13-08 y Antonio Cabrera, El Coca, Ecuador", // Texto para SEO
         googleMapsEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19514.146046953483!2d-78.951733654179!3d-2.549799250875259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91cd671fef3cc28f%3A0x9c2dd46921b71674!2sTaller%20De%20Carpinter%C3%ADa%20Quiroz!5e1!3m2!1ses!2sec!4v1775443965239!5m2!1ses!2sec"
     },
